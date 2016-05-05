e3ac82b9f2167136207f7d60998155689670a3a4
Rojar Smith <rojarsmith@gmail.com>
2016-05-06 00:14:21 +0800

    +Add java reflection.

---
+++ Eclipse/Java_Reflection/src/lab/PersionData.java
@@ -0,0 +1,15 @@
+package lab;
+
+public class PersionData extends Persion{
+
+	public PersionData(String name, int age) {
+		super(name, age);		
+	}
+
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Eclipse/Java_Reflection/src/lab/Tom.java
@@ -0,0 +1,13 @@
+package lab;
+
+public class Tom extends Persion {
+
+	public Tom(String name, int age) {
+		super(name, age);
+	}
+
+	public void sayHello() {
+		System.out.println("Yes, " + getName() + " say hello, age=" + getAge());
+	}
+
+}
