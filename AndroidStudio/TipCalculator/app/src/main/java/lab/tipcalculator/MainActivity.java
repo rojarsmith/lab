52d02bae601a7ae00228034720277fcda95129f3
Rojar Smith <rojarsmith@wearesoft.com>
2016-06-17 21:53:11 +0800

    +Code

--- AndroidStudio/TipCalculator/app/src/main/java/lab/tipcalculator/MainActivity.java
+++ AndroidStudio/TipCalculator/app/src/main/java/lab/tipcalculator/MainActivity.java
@@ -2,12 +2,98 @@ package lab.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getNumberInstance();
+    private double billAmount = 0.0; // bill amount entered by the user
+    private double percent = 0.15; // initial tip percentage
+    private TextView amountTextView; // shows formatted bill amount
+    private TextView percentTextView; // shows tip percentage
+    private TextView tipTextView; // shows calculated tip amount
+    private TextView totalTextView; // shows calculated total bill amount
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));
+
+        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
+        amountEditText.addTextChangedListener(amountEditTextWatcher);
+
+        SeekBar percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+        percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
     }
+
+    // calculate and display tip and total amounts
+    private void calculate() {
+        // format percent and display in percentTextView
+        percentTextView.setText(percentFormat.format(percent));
+
+        // calculate the tip and total
+        double tip = billAmount * percent;
+        double total = billAmount + tip;
+
+        // display tip and total formatted as currency
+        tipTextView.setText(currencyFormat.format(tip));
+        totalTextView.setText(currencyFormat.format(total));
+    }
+
+    private final OnSeekBarChangeListener seekBarListener =
+            new OnSeekBarChangeListener() {
+                // update percent, then call calculate
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    percent = progress / 100.0; // set percent based on progress
+                    calculate(); // calculate and display tip and total
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            };
+
+    private final TextWatcher amountEditTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                billAmount = Double.parseDouble(s.toString()) / 100.0;
+                amountTextView.setText(currencyFormat.format(billAmount));
+            } catch (NumberFormatException e) {
+                amountTextView.setText("");
+                billAmount = 0.0;
+            }
+
+            calculate();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
