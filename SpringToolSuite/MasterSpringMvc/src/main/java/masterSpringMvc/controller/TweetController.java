74cbe65952f9b9aa497eaaee6ef211040fb47f94
Rojar Smith <rojarsmith@gmail.com>
2016-09-19 19:59:34 +0800

    +Update training.

--- SpringToolSuite/MasterSpringMvc/src/main/java/masterSpringMvc/controller/TweetController.java
+++ SpringToolSuite/MasterSpringMvc/src/main/java/masterSpringMvc/controller/TweetController.java
@@ -3,6 +3,8 @@ package masterSpringMvc.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -10,7 +12,9 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
@@ -18,23 +22,28 @@ public class TweetController {
 	@Autowired
 	private Twitter twitter;
 
-	@RequestMapping("/showTwitter2")
-	public String showTwitter2(@RequestParam(defaultValue = "RojarSmith") String search, Model model){
-		SearchResults searchResult = twitter.searchOperations().search(search);
-		List<Tweet> messages = searchResult.getTweets();		
-		model.addAttribute("tweets", messages);
-		model.addAttribute("search", search);
-		return "resultTwitter2";
+	@RequestMapping("/")
+	public String home(){
+		return "searchPage";
+	}
+
+	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+		String search = request.getParameter("search");
+		if(search.toLowerCase().contains("struts")){
+			redirectAttributes.addFlashAttribute("error", "Try using spring instead!");
+			return "redirect:/";
+		}
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:/result";		
 	}
 	
-	@RequestMapping("/showTwitter")
-	public String showTwitter(@RequestParam(defaultValue = "RojarSmith") String search, Model model){
+	@RequestMapping("/result")
+	public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model){
 		SearchResults searchResult = twitter.searchOperations().search(search);
-		List<String> messages = searchResult.getTweets()
-				.stream()
-				.map(Tweet::getText)
-				.collect(Collectors.toList());		
-		model.addAttribute("tweets", messages);
-		return "resultTwitter";
+		List<Tweet> tweets = searchResult.getTweets();		
+		model.addAttribute("tweets", tweets);
+		model.addAttribute("search", search);
+		return "resultPage";
 	}
 }
