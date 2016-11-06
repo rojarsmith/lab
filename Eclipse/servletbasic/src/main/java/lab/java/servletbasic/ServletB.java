2b266170a51b8ba6e6624c2d83f222083484e4ab
Rojar Smith <rojarsmith@gmail.com>
2016-11-06 21:06:31 +0800

    +Add servletbasic.

---
+++ Eclipse/servletbasic/src/main/java/lab/java/servletbasic/ServletB.java
@@ -0,0 +1,23 @@
+package lab.java.servletbasic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class ServletB extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void service(ServletRequest req, ServletResponse res) throws IOException, ServletException {
+
+		res.setContentType("text/html");
+
+		PrintWriter out = res.getWriter();
+		out.print("<html><body>");
+		out.print("<b>hello generic servlet</b>");
+		out.print("</body></html>");
+	}
+}
