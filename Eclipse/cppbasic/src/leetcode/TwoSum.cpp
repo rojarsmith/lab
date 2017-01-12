88802e95c2933acaee30c188946d57eebb005be5
Rojar Smith <rojarsmith@gmail.com>
2017-01-12 20:41:37 +0800

    +Add LeetCode - Two Sum.

---
+++ Eclipse/cppbasic/src/leetcode/TwoSum.cpp
@@ -0,0 +1,26 @@
+/*
+ * TwoSum.cpp
+ *
+ *  Created on: 2017年1月11日
+ *      Author: rojar
+ */
+
+#include "TwoSum.h"
+
+vector<int> TwoSum::twoSum(vector<int>& nums, int target){
+	return nums;
+}
+
+
+TwoSum::TwoSum() {
+	int array[] = {2,7,11,15};
+	vector<int> v(4);
+	//v.assign(array, array+4);
+
+
+}
+
+TwoSum::~TwoSum() {
+	// TODO Auto-generated destructor stub
+}
+
