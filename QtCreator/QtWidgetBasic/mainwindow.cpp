79c3fc6b8191ff238fd1cd7ffa29637cacd808f5
rojarsmith <rojarsmith@gmail.com>
2017-08-11 14:14:37 +0800

    +Add size setting.

--- QtCreator/QtWidgetBasic/mainwindow.cpp
+++ QtCreator/QtWidgetBasic/mainwindow.cpp
@@ -3,6 +3,10 @@
 #include <QtWidgets>
 #include <QGraphicsScene>
 
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf-8")
+#endif
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
@@ -11,8 +15,38 @@ MainWindow::MainWindow(QWidget *parent) :
 
 {
     //ui->setupUi(this);
+    setWindowTitle(tr("Macaca"));
+    setFixedSize(160*6, 90*6);
     graphicsView->setScene(graphicsScene);
     setCentralWidget(graphicsView);
+
+    QDockWidget *dockToolBox = new QDockWidget(tr("Toolbox"), this);
+    dockToolBox->setFeatures(QDockWidget::DockWidgetMovable);
+    dockToolBox->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+    dockToolBox->setMinimumWidth(150);
+    addDockWidget(Qt::LeftDockWidgetArea, dockToolBox);
+
+    QDockWidget *dockProperty = new QDockWidget(tr("Property"), this);
+    dockProperty->setFeatures(QDockWidget::DockWidgetMovable);
+    dockProperty->setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+    dockProperty->setMinimumWidth(150);
+    addDockWidget(Qt::RightDockWidgetArea, dockProperty);
+
+    QDockWidget *dockPage = new QDockWidget(tr("Page"), this);
+    dockPage->setFeatures(QDockWidget::DockWidgetMovable);
+    dockPage->setAllowedAreas(Qt::BottomDockWidgetArea);
+    dockPage->setMinimumHeight(90);
+    addDockWidget(Qt::BottomDockWidgetArea, dockPage);
+
+    QDockWidget *dockConsole = new QDockWidget(tr("Console"), this);
+    dockConsole->setFeatures(QDockWidget::DockWidgetMovable);
+    dockConsole->setAllowedAreas(Qt::BottomDockWidgetArea);
+    dockPage->setMinimumHeight(90);
+    addDockWidget(Qt::BottomDockWidgetArea, dockConsole);
+
+    tabifyDockWidget(dockPage, dockConsole);
+
+    statusBar()->showMessage(tr("Test message"));
 
     createActions();
 }
@@ -28,9 +62,9 @@ void MainWindow::createActions()
     QToolBar *fileToolBar = addToolBar(tr("File"));
 
     const QIcon newIcon = QIcon::fromTheme("document-new", QIcon(":/images/new.png"));
-    QAction *newLetterAct = new QAction(newIcon, tr("&New Letter"), this);
+    QAction *newLetterAct = new QAction(newIcon, tr("&New Project"), this);
     newLetterAct->setShortcuts(QKeySequence::New);
-    newLetterAct->setStatusTip(tr("Create a new form letter"));
+    newLetterAct->setStatusTip(tr("Create a new project"));
     fileMenu->addAction(newLetterAct);
     fileToolBar->addAction(newLetterAct);
 }
