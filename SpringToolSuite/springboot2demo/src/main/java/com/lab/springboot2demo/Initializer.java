f0b2bc822d892374969b0d8856b5125233d7aea4
Rojar Smith <rojarsmith@gmail.com>
2020-04-02 12:29:06 +0800

    - Add Spring demo. - Add nodejs demo.

---
+++ SpringToolSuite/springboot2demo/src/main/java/com/lab/springboot2demo/Initializer.java
@@ -0,0 +1,43 @@
+package com.lab.springboot2demo;
+
+import com.lab.springboot2demo.model.Event;
+import com.lab.springboot2demo.model.Group;
+import com.lab.springboot2demo.model.GroupRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+@Component
+class Initializer implements CommandLineRunner {
+
+    private final GroupRepository repository;
+
+    public Initializer(GroupRepository repository) {
+        this.repository = repository;
+    }
+    
+	@Override
+	public void run(String... args) throws Exception {
+        Stream.of(
+        "Denver JUG",
+        "Utah JUG",
+        "Seattle JUG",
+        "Richmond JUG")
+        .forEach(name -> repository.save(new Group(name)));
+
+        Group djug = repository.findByName("Denver JUG");
+        Event e = Event.builder().title("Full Stack Reactive")
+                .description("Reactive with Spring Boot + React")
+                .date(Instant.parse("2018-12-12T18:00:00.000Z"))
+                .build();
+        djug.setEvents(Collections.singleton(e));
+        repository.save(djug);
+
+        repository.findAll().forEach(System.out::println);
+	}
+
+}
