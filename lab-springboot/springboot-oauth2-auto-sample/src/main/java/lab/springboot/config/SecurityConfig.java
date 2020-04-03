a7e26d8f477123282c83c0b4f80040b6b04e2559
Rojar Smith <rojarsmith@gmail.com>
2020-04-03 16:08:54 +0800

    - Add simple resource server.

--- lab-springboot/springboot-oauth2-auto-sample/src/main/java/lab/springboot/config/SecurityConfig.java
+++ lab-springboot/springboot-oauth2-auto-sample/src/main/java/lab/springboot/config/SecurityConfig.java
@@ -1,9 +1,44 @@
 package lab.springboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    /**
+     * Must in Spring 5.
+     *
+     * @return BCryptPasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    /**
+     * Create 3 user
+     *
+     * @return InMemoryUserDetailsManager
+     */
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("123")
+                .password(passwordEncoder().encode("1234"))
+                .authorities("ROLE_USER").build());
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder().encode("123456"))
+                .authorities("ROLE_USER").build());
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .authorities("ROLE_ADMIN").build());
+        return manager;
+    }
 }
