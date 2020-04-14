6313fd1a6ee9cb9d298012502515ed05016966d0
Rojar Smith <rojarsmith@gmail.com>
2020-04-14 13:30:48 +0800

    - Add resource service example.

--- lab-springboot/springboot-security-oauth2-authorization/src/main/java/lab/springboot/authorization/config/Oauth2AuthorizationServerConfig.java
+++ lab-springboot/springboot-security-oauth2-authorization/src/main/java/lab/springboot/authorization/config/Oauth2AuthorizationServerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 
 import lombok.NonNull;
@@ -23,44 +24,43 @@ import lombok.RequiredArgsConstructor;
 public class Oauth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	@Autowired
 	private @NonNull AuthenticationManager authenticationManager;
-
 	private @NonNull ClientDetails clientDetails;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//		InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
-//		builder
-//		// 構建一個 id 為 oauth2 的客戶端
-//        .withClient("oauth2")
-//        // 設置她的密鑰，加密後的
-//        .secret("$2a$10$K74U3q/EBh/cO2UmFsxC7.ffxbhjH/Gdw0lNh02ANeoVole73gSLS")
-//        // 設置允許訪問的資源 id
-//        .resourceIds("oauth2")
-//        // 授權的類型
-//        .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-//        // 可以授權的角色
-//        .authorities("ROLE_ADMIN", "ROLE_USER")
-//        // 授權的範圍
-//        .scopes("all")
-//        // token 有效期
-//        .accessTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
-//        // 刷新 token 的有效期
-//        .refreshTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
-//        // 授權碼模式的重定向地址
-//        .redirectUris("http://example.com");
-//		
-//        builder
-//        .withClient("test")
-//        .secret("$2a$10$45FQBOnTRERkvav.sNAroeJkEaFLZrLzRC2qrMVIdQnLKCTL8z/9q")
-//        .resourceIds("test")
-//        .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-//        .authorities("ROLE_ADMIN", "ROLE_USER")
-//        .scopes("all")
-//        .accessTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
-//        .refreshTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
-//        .redirectUris("http://example.com");
+		InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+		builder
+		// 構建一個 id 為 oauth2 的客戶端
+        .withClient("oauth2")
+        // 設置她的密鑰，加密後的
+        .secret("$2a$10$K74U3q/EBh/cO2UmFsxC7.ffxbhjH/Gdw0lNh02ANeoVole73gSLS")
+        // 設置允許訪問的資源 id
+        .resourceIds("oauth2")
+        // 授權的類型
+        .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+        // 可以授權的角色
+        .authorities("ROLE_ADMIN", "ROLE_USER")
+        // 授權的範圍
+        .scopes("all")
+        // token 有效期
+        .accessTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
+        // 刷新 token 的有效期
+        .refreshTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
+        // 授權碼模式的重定向地址
+        .redirectUris("http://example.com");
+		
+        builder
+        .withClient("test")
+        .secret("$2a$10$45FQBOnTRERkvav.sNAroeJkEaFLZrLzRC2qrMVIdQnLKCTL8z/9q")
+        .resourceIds("test")
+        .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+        .authorities("ROLE_ADMIN", "ROLE_USER")
+        .scopes("all")
+        .accessTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
+        .refreshTokenValiditySeconds(Math.toIntExact(Duration.ofHours(1).getSeconds()))
+        .redirectUris("http://example.com");
 
-		configClient(clients);
+		//configClient(clients);
 	}
 
 	private void configClient(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -91,4 +91,9 @@ public class Oauth2AuthorizationServerConfig extends AuthorizationServerConfigur
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(this.authenticationManager);
 	}
+	
+	@Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security.checkTokenAccess("isAuthenticated()");
+    }	
 }
