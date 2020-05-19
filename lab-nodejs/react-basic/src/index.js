b7fb37e0444018c52e04b85b1abc8446164419b7
Rojar Smith <rojarsmith@gmail.com>
2020-05-19 17:03:08 +0800

    - Add button pass value.

--- lab-nodejs/react-basic/src/index.js
+++ lab-nodejs/react-basic/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const multiButton = () => {
+  var output=[];
+  for(let i=0;i<4;++i){
+    output.push(<button>{i}</button>)
+  }
+  return output;
+}
+
+const getValue = (event) => {
+  console.log(event.target.value)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    { multiButton() }
+    <button value={true} onClick={getValue}>Get value</button>
   </React.StrictMode>,
   document.getElementById('root')
 );
