e4e54a6ab72ca79e52338a95f10d47f4d9165b88
Rojar Smith <rojarsmith@gmail.com>
2020-10-05 10:35:45 +0800

    Add timer example.

--- lab-nodejs/basic/index.js
+++ lab-nodejs/basic/index.js
@@ -1,1 +1,10 @@
 console.log("Hello world!");
+
+var timer = require('./timer');
+console.log(timer);
+// timer.timer();
+timer.timer2();
+timer.timer3();
+timer.timer4();
+timer.timer5();
+timer.timer6();
