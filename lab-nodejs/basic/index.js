4c3702266c0ab50b4bc10091a73e9b7ae5ddbbb0
Rojar Smith <rojarsmith@gmail.com>
2020-10-15 22:35:54 +0800

    Add curried function.

--- lab-nodejs/basic/index.js
+++ lab-nodejs/basic/index.js
@@ -1,5 +1,18 @@
 console.log("Hello world!");
 
+// curried function
+const add = x => y => x + y;
+console.log(add(1)(2));
+
+const add2 = function (x) {
+    return function (y) {
+        return x + y
+    }
+}
+
+console.log(add2(1)(2));
+
+// timer
 var timer = require('./timer');
 console.log(timer);
 // timer.timer();
