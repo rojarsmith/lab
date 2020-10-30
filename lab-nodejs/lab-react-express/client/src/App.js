349c9bf1c9ac5181f63be3d27d9da85d8106b686
Rojar Smith <rojarsmith@gmail.com>
2020-10-30 22:26:49 +0800

    Add the growing react component.

--- lab-nodejs/lab-react-express/client/src/App.js
+++ lab-nodejs/lab-react-express/client/src/App.js
@@ -11,12 +11,15 @@ const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
 
 const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
 
+const TodoItem = ({ todo }) => <p>id: {todo.id} todo: {todo}</p>
+
 const CurrenciesWithAmount = withAmount([Euro, Pound]);
 
 // Config proxy parameter in the package.json avoid CROS.
 // "proxy": "http://localhost:5020"
 function App() {
   const [amount, setAmount] = useState(0);
+  const [todos, setTodos] = useState();
 
   useEffect(() => {
     fetch('/api')
@@ -30,6 +33,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TodoList todos={todos} />
         <Amount
           renderAmountOne={amount => (
             <div>
@@ -50,6 +54,34 @@ function App() {
         Hoc
         <CurrenciesWithAmount />
       </header>
+    </div>
+  );
+}
+
+function TodoList({ todos, isLoadingTodos }) {
+  if (isLoadingTodos) {
+    return (
+      <div>
+        <p>Loading todos ...</p>
+      </div>
+    );
+  }
+
+  if (!todos) {
+    return null;
+  }
+
+  if (!todos.length) {
+    return (
+      <div>
+        <p>You have no Todos.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {todos.map(todo => <li key={todo.id} todo={todo} />)}
     </div>
   );
 }
