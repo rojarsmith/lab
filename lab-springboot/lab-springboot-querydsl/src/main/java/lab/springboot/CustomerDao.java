6b64b3ff3aa7f861ec047001d1a4adf4ae2065ad
Rojar Smith <rojarsmith@gmail.com>
2021-04-12 14:01:30 +0800

    Add the condition example with QueryDSL.

--- lab-springboot/lab-springboot-querydsl/src/main/java/lab/springboot/CustomerDao.java
+++ lab-springboot/lab-springboot-querydsl/src/main/java/lab/springboot/CustomerDao.java
@@ -2,9 +2,11 @@ package lab.springboot;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Service
@@ -15,9 +17,17 @@ public class CustomerDao {
 	public List<Customer> findCustomer(String firstName, String lastName) {
 		QCustomer qcustomer = QCustomer.customer;
 		
+		BooleanBuilder condition = new BooleanBuilder();
+		if(StringUtils.isBlank(firstName) || StringUtils.isBlank(lastName)) {
+			condition.and(qcustomer.firstName.eq(firstName))
+			.or(qcustomer.lastName.eq(lastName));
+		} else {
+			condition.and(qcustomer.firstName.eq(firstName))
+			.and(qcustomer.lastName.eq(lastName));
+		}
+		
 		return queryFactory.selectFrom(qcustomer)
-				.where(qcustomer.firstName.eq(firstName)
-						.or(qcustomer.lastName.eq(lastName)))
+				.where(condition)
 				.fetch();
 	}
 }
